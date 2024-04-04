6270e32f4d4cc8ea94cacf6d2099983f28327321
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2024-04-04 15:02:30 +0700

    step 5

--- algortimasearch.cpp
+++ algortimasearch.cpp
@@ -52,6 +52,12 @@ void LinearSearch()
             }
             i++; //step 4
         }
+
+        if (i == n) // step 5
+        {
+            
+        }
+        
         
     } while ();
 }
