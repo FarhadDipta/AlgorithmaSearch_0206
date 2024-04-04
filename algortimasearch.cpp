b398374016894fbaa26775de50a1eadd7689b754
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2024-04-04 14:49:11 +0700

    void input

--- algortimasearch.cpp
+++ algortimasearch.cpp
@@ -5,6 +5,29 @@ int arr[20];
 int n;
 int i;
 
+void input() {
+    while (true)
+    {
+        cout << "Masukan panjang elemnt array : ";
+        cin >> n;
+
+        if (n <+ 20)
+            break;
+        else
+            cout << "\nMaksimum panjang array adalah 20" << endl;
+    }
+
+    cout << "\n--------------------" << endl;
+    cout << "\nEnter Array Elemnt" << endl;
+    cout << "\n--------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "<" << (i + 1) << ">";
+        cin >> arr[i];
+    }
+}
+
 int main(){
 
 }
